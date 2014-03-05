41468bbf7569dc50efb75b47bdd8999e48f0cda9
Kurt Gardiner <k.gardiner@nib.com.au>
2014-03-05 13:43:21 +1100

    Put the license file in the bin dir

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class qas_batch::config inherits qas_batch {
   }
 
   file {
-    "${::install_dir}/qalicn.ini":
+    "${::install_dir}/bin/qalicn.ini":
       ensure  => 'present',
       content => template('qas_batch/qalicn.ini.erb');
   }
